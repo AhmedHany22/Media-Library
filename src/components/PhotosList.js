45870baeea1c9646807e80f2de5cc279599fb54a
Ahmed <ahmed_hany19@outlook.com>
2023-01-16 20:35:04 +0200

    Connect photosApi to "Store", Reducer, middleware

--- src/components/PhotosList.js
+++ src/components/PhotosList.js
@@ -1,5 +1,24 @@
+import { useFetchPhotosQuery, useAddPhotoMutation } from '../store';
+import Button from './Button';
+
 function PhotosList({ album }) {
-  return 'PhotosList';
+  useFetchPhotosQuery(album);
+  const [addPhoto, addPhotoResults] = useAddPhotoMutation();
+
+  const handleAddPhoto = () => {
+    addPhoto(album);
+  };
+
+  return (
+    <div>
+      <div className="m-2 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">Photos In {album.title}</h3>
+        <Button loading={addPhotoResults.isLoading} onClick={handleAddPhoto}>
+          + Add Photo
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default PhotosList;
