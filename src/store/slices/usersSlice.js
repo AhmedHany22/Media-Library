3ae2de08bc9961f42027d54e3bcb5c8f0695f4fa
Ahmed <ahmed_hany19@outlook.com>
2023-01-16 16:27:34 +0200

    Setup Redux-Toolkit + create "user" slice

---
+++ src/store/slices/usersSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState: {
+    data: [],
+    isLoading: false,
+    error: null,
+  },
+  reducers: {},
+});
+
+export const usersReducer = usersSlice.reducer;
