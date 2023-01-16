cb3238436d94d0ef9ed4d3018c042d75c57e8cb6
Ahmed <ahmed_hany19@outlook.com>
2023-01-16 16:38:33 +0200

    Adding a Pause for Testing the usersFetch method

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -1,10 +1,20 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const fetchUsers = createAsyncThunk('users/fetch', async () => {
-  const response = await axios.get('http://localhost:3005/users');
+const fetchUsers = createAsyncThunk("users/fetch", async () => {
+  const response = await axios.get("http://localhost:3005/users");
+
+  // DEV ONLY!!!
+  await pause(5000);
 
   return response.data;
 });
+
+// DEV ONLY!!!
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 export { fetchUsers };
