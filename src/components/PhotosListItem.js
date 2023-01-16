d886abad88d6c3b2504b53cdb49c69a2ad8f3d28
Ahmed <ahmed_hany19@outlook.com>
2023-01-16 20:36:04 +0200

    Showing the List of Photos

--- src/components/PhotosListItem.js
+++ src/components/PhotosListItem.js
@@ -1,5 +1,9 @@
-function PhotosListItem() {
-  return 'PhotosListItem';
+function PhotosListItem({ photo }) {
+  return (
+    <div>
+      <img className="h-20 w-20" src={photo.url} alt="random pic" />
+    </div>
+  );
 }
 
 export default PhotosListItem;
