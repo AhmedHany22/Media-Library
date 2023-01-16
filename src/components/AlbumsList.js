0d4ee44388912fd2aa5187a026329d50eb1d2607
Ahmed <ahmed_hany19@outlook.com>
2023-01-16 19:49:10 +0200

    A Few Immediate Notes

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,9 +1,10 @@
 import { useFetchAlbumsQuery } from '../store';
 
 function AlbumsList({ user }) {
-  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const results = useFetchAlbumsQuery(user);
 
-  console.log(data, error, isLoading);
+  console.log(results);
+  // console.log(data, error, isLoading);
 
   return <div>Albums for {user.name}</div>;
 }
