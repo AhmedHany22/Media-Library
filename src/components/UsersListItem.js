a0975b203bb18c80a5b97fc732de26cf5e6d50fe
Ahmed <ahmed_hany19@outlook.com>
2023-01-16 18:59:37 +0200

    Implement Remove-User

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -1,7 +1,22 @@
+import { GoTrashcan } from 'react-icons/go';
+import Button from './Button';
+import { removeUser } from '../store';
+import { useThunk } from '../hooks/use-thunk';
+
 function UsersListItem({ user }) {
+  const [doRemoveUser, isLoading, error] = useThunk(removeUser);
+
+  const handleClick = () => {
+    doRemoveUser(user);
+  };
+
   return (
     <div className="mb-2 border rounded">
       <div className="flex p-2 justify-between items-center cursor-pointer">
+        <Button loading={isLoading} onClick={handleClick}>
+          <GoTrashcan />
+        </Button>
+        {error && <div>Error deleting user.</div>}
         {user.name}
       </div>
     </div>
